8554049df29b096c4106999a9143f9ae8c7bf8b0
Harrison Pan <hanshen36@gmail.com>
2016-03-20 01:39:43 +1300

    Make two java files work together
    Signed-off-by: Harrison Pan <hanshen36@gmail.com>

--- helloworld/src/main/java/com/harrison/helloworld/App.java
+++ helloworld/src/main/java/com/harrison/helloworld/App.java
@@ -6,8 +6,25 @@ package com.harrison.helloworld;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World + modify #1!" );
+	private Output op1;
+	
+    public Output getOp1() {
+		return op1;
+	}
+
+	public void setOp1(Output op1) {
+		this.op1 = op1;
+	}
+	
+	public void setMsg() {
+		op1.setMsg("Hello World");
+	}
+	
+	public String printMsg() {
+		return op1.getMsg();
+	}
+
+	public static void main( String[] args ) {
+        System.out.println("Hello World");
     }
 }
--- helloworld/src/main/java/com/harrison/helloworld/Output.java
+++ helloworld/src/main/java/com/harrison/helloworld/Output.java
@@ -1,7 +1,13 @@
 package com.harrison.helloworld;
 
 public class Output {
-	public void printMsg() {
-		System.out.println( "Hello World + modify #1!" );
+	private String msg;
+	
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
 	}
 }
