6e9768e8cdd0787c4487e03bc4d67b2e451da94f
Harrison Pan <hanshen36@gmail.com>
2016-03-20 23:10:26 +1300

    Final edition to App.java
    Signed-off-by: Harrison Pan <hanshen36@gmail.com>

--- helloworld/src/main/java/com/harrison/helloworld/App.java
+++ helloworld/src/main/java/com/harrison/helloworld/App.java
@@ -6,25 +6,9 @@ package com.harrison.helloworld;
  */
 public class App 
 {
-	private Output op1;
-	
-    public Output getOp1() {
-		return op1;
-	}
-
-	public void setOp1(Output op1) {
-		this.op1 = op1;
-	}
-	
-	public void setMsg() {
-		op1.setMsg("Hello World");
-	}
-	
-	public String printMsg() {
-		return op1.getMsg();
-	}
-
 	public static void main( String[] args ) {
-        System.out.println("Hello World");
+		Output op = new Output();
+		op.setMsg("Hello World");
+        System.out.println(op.getMsg());
     }
 }
